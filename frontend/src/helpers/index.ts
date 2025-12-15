6c737078047a911874f9acbfb3934065c58250cc
Aviral2442 <aviralsrivastava24y15@gmail.com>
2025-12-15 16:10:18 +0530

    refactor: update application name and title to DEHS

--- frontend/src/helpers/index.ts
+++ frontend/src/helpers/index.ts
@@ -4,11 +4,11 @@ export const currency: CurrencyType = '$'
 
 export const currentYear = new Date().getFullYear()
 
-export const appName = 'Medcab'
-export const appTitle = 'Medcab - Admin Dashboard'
+export const appName = 'DEHS'
+export const appTitle = 'DEHS - Digital Education Health System'
 export const appDescription: string =
-  'Medcab is a premium admin dashboard template built with React and TypeScript.'
-export const author: string = 'Medcab'
+  'DEHS stands for Digital Education Health System'
+export const author: string = 'DEHS'
 export const authorWebsite: string = 'IME.com'
 export const authorContact: string = ''
 
