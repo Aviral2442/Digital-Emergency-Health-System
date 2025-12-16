ad966b618596b3bf9702fcfd24ea85b05efa45e9
Aviral2442 <aviralsrivastava24y15@gmail.com>
2025-12-16 17:27:48 +0530

    feat: remove unused ambulance booking list services and controllers

--- backend/src/routes/ambulance.routes.ts
+++ backend/src/routes/ambulance.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { ambulanceBookingDetailController, ambulanceBookingCountController, getAmbulanceBookingListController, getBulkAmbulanceBookingListController, getRegularAmbulanceBookingListController, getRentalAmbulanceBookingListController, ambulanceCompleteOngoingCancelReminderBookingCountsController } from '../controllers/ambulance.controller';
+import { ambulanceBookingDetailController, ambulanceBookingCountController, getAmbulanceBookingListController, ambulanceCompleteOngoingCancelReminderBookingCountsController } from '../controllers/ambulance.controller';
 
 const router = Router();
 
@@ -9,9 +9,6 @@ router.get('/ambulance_booking_count', ambulanceCompleteOngoingCancelReminderBoo
 
 // BOOKING & DETAILS ROUTES 
 router.get('/get_ambulance_booking_list', getAmbulanceBookingListController);
-router.get('/get_regular_ambulance_booking_list', getRegularAmbulanceBookingListController);
-router.get('/get_rental_ambulance_booking_list', getRentalAmbulanceBookingListController);
-router.get('/get_bulk_ambulance_booking_list', getBulkAmbulanceBookingListController);
 router.get('/ambulance_booking_detail/:id', ambulanceBookingDetailController);
 
 export default router;
